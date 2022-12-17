7aeeb7a29414734544a7f78be09b3c4e4715e21c
Praveen Katti <Praveen_Katti@Dellteam.com>
2022-12-17 16:32:52 +0530

    API First Commit

--- Program.cs
+++ Program.cs
@@ -10,10 +10,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 app.MapControllers();
 
-app.Run();
+app.Run(); 
 //on 14th video
